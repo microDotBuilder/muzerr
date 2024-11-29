3b5dec9cd425a963a4ca43a6b6825e28cd04e2f7
boringtutor <micro.builder.io@gmail.com>
2024-11-29 13:22:51 -0800

    ADDED auth with clerk

--- FrontEnd/src/env/server.ts
+++ FrontEnd/src/env/server.ts
@@ -7,6 +7,10 @@ export const env = createEnv({
 
   server: {
     NODE_ENV: z.enum(["development", "production"]),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1),
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1),
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1),
   },
   onValidationError: (error: ZodError) => {
     console.error(
